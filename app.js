22260569c969d9af8af5d8c603b2c6255ea54139
Mohammed Farhan <mohammednm40@gmail.com>
2021-01-17 15:51:17 +0200

    Add pug

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const app = express();
 
 
 const port = process.env.PORT || 3003;
-app.listen(port, () => {
-  console.log(`Server is runing a http://localhost:${port}`);
+
+const server = app.listen(port, () => {
+  console.log(`Server is runing at http://localhost:${port}`);
+});
+
+app.set("view engine", "Pug");
+app.set("views", "views");
+
+app.get("/", (req, res, next) => {
+  res.status(200).render("home");
 });
