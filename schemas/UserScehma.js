bc83850be0286eaecf9e2c74331f9c20e5485c13
Mohammed Farhan <mohammednm40@gmail.com>
2021-01-18 17:43:19 +0200

    Create User Schema

---
+++ schemas/UserScehma.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const Scehma = mongoose.Schema;
+
+const UserScehma = new Scehma({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    username:{
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    profilePic: {
+        type: String,
+        default: "/images/profilePic.png",
+    },
+});
+
+const User = mongoose.model('User', UserScehma);
+module.exports = User;
