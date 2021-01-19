ac78c559db92cbac500d832d1355743b4a954ffe
Mohammed Farhan <mohammednm40@gmail.com>
2021-01-19 18:31:48 +0200

    Adding click handelr to the post button

--- public/js/common.js
+++ public/js/common.js
@@ -4,10 +4,23 @@ $("#postTextarea").keyup(event => {
     const submitButton = $("#submitPostButton");
 
     if (submitButton.length == 0) return alert("No sumbit")
-    if(vlaue == ""){
+    if (vlaue == "") {
         submitButton.prop("disabled", true);
         return;
     }
 
     submitButton.prop("disabled", false)
-})
+});
+
+$("#submitPostButton").click((event) => {
+    const button = $(event.target);
+    const textbox = $("#postTextarea");
+
+    const data = {
+        content: textbox.val(),
+    }
+
+    $.post("/api/post", data, (postDAta, status, xhr) => {
+        
+    })
+});
