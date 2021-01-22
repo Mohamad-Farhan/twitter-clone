f0b6552f6341bdc1d4c0f2b14713a382f5c40827
Mohammed Farhan <mohammednm40@gmail.com>
2021-01-22 18:54:22 +0200

    Ending the outputting posts onto the news feed

--- public/js/common.js
+++ public/js/common.js
@@ -31,7 +31,7 @@ $("#submitPostButton").click((event) => {
     })
 })
 
-function createPostHtml(postData) {
+const createPostHtml = (postData) => {
 
     const postedBy = postData.postedBy;
 
@@ -40,7 +40,7 @@ function createPostHtml(postData) {
     }
 
     const displayName = postedBy.firstName + " " + postedBy.lastName;
-    const timestamp = postData.createdAt;
+    const timestamp = timeDifference(new Date(), new Date(postData.createdAt));
 
     return `<div class='post'>
 
@@ -77,4 +77,40 @@ function createPostHtml(postData) {
                     </div>
                 </div>
             </div>`;
+}
+
+const timeDifference = (current, previous) => {
+
+    const msPerMinute = 60 * 1000;
+    const msPerHour = msPerMinute * 60;
+    const msPerDay = msPerHour * 24;
+    const msPerMonth = msPerDay * 30;
+    const msPerYear = msPerDay * 365;
+
+    const elapsed = current - previous;
+
+    if (elapsed < msPerMinute) {
+        if (elapsed / 1000 < 30) return "Just now";
+        return Math.round(elapsed / 1000) + ' seconds ago';
+    }
+
+    else if (elapsed < msPerHour) {
+        return Math.round(elapsed / msPerMinute) + ' minutes ago';
+    }
+
+    else if (elapsed < msPerDay) {
+        return Math.round(elapsed / msPerHour) + ' hours ago';
+    }
+
+    else if (elapsed < msPerMonth) {
+        return Math.round(elapsed / msPerDay) + ' days ago';
+    }
+
+    else if (elapsed < msPerYear) {
+        return Math.round(elapsed / msPerMonth) + ' months ago';
+    }
+
+    else {
+        return Math.round(elapsed / msPerYear) + ' years ago';
+    }
 }
