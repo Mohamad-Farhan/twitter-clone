c84af795c56b97531ed7cd2a6794a2ab58c50403
Mohammed Farhan <mohammednm40@gmail.com>
2021-01-23 20:33:39 +0200

    Updating the button to show number of likes

--- public/js/common.js
+++ public/js/common.js
@@ -14,7 +14,7 @@ $("#postTextarea").keyup(event => {
     submitButton.prop("disabled", false);
 })
 
-$("#submitPostButton").click((event) => {
+$("#submitPostButton").click(() => {
     const button = $(event.target);
     const textbox = $("#postTextarea");
 
@@ -41,7 +41,9 @@ $(document).on("click", ".likeButton", (event) => {
         url: `/api/posts/${postId}/like`,
         type: "PUT",
         success: (postData) => {
-            console.log(postData.likes.length);
+
+            button.find("span").text(postData.likes.length || "");
+
         }
     })
 
@@ -97,6 +99,7 @@ function createPostHtml(postData) {
                             <div class='postButtonContainer'>
                                 <button class='likeButton'>
                                     <i class='far fa-heart'></i>
+                                    <span>${postData.likes.length || ""}</span>
                                 </button>
                             </div>
                         </div>
