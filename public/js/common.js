80c7bf2903e09766f8a7012d8ad25c72faaa88d9
Mohammed Farhan <mohammednm40@gmail.com>
2021-01-23 22:52:06 +0200

    End the like button

--- public/js/common.js
+++ public/js/common.js
@@ -41,8 +41,16 @@ $(document).on("click", ".likeButton", (event) => {
         url: `/api/posts/${postId}/like`,
         type: "PUT",
         success: (postData) => {
+
             button.find("span").text(postData.likes.length || "");
-            
+
+            if (postData.likes.includes(userLoggedIn._id)) {
+                button.addClass("active");
+            }
+            else {
+                button.removeClass("active");
+            }
+
         }
     })
 
@@ -68,6 +76,7 @@ function createPostHtml(postData) {
 
     const displayName = postedBy.firstName + " " + postedBy.lastName;
     const timestamp = timeDifference(new Date(), new Date(postData.createdAt));
+    const likeButtonActiveClass = postData.likes.includes(userLoggedIn._id) ? 'active' : '';
 
     return `<div class='post' data-id='${postData._id}'>
 
@@ -90,13 +99,13 @@ function createPostHtml(postData) {
                                     <i class='far fa-comment'></i>
                                 </button>
                             </div>
-                            <div class='postButtonContainer'>
-                                <button>
+                            <div class='postButtonContainer green'>
+                                <button class='retweet'>
                                     <i class='fas fa-retweet'></i>
                                 </button>
                             </div>
-                            <div class='postButtonContainer'>
-                                <button class='likeButton'>
+                            <div class='postButtonContainer red'>
+                                <button class='likeButton ${likeButtonActiveClass}'>
                                     <i class='far fa-heart'></i>
                                     <span>${postData.likes.length || ""}</span>
                                 </button>
