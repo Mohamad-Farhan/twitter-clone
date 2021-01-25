ae22ae971b48d39611b3486b5cfeca73a3dfa662
Mohammed Farhan <mohammednm40@gmail.com>
2021-01-25 14:25:55 +0200

    Enabling the reply Button when the user type

--- public/js/common.js
+++ public/js/common.js
@@ -2,6 +2,8 @@ $("#postTextarea").keyup(event => {
     const textbox = $(event.target);
     const value = textbox.val().trim();
 
+    let isModal = textbox.parents(".modal").length == 1;
+
     const submitButton = $("#submitPostButton");
 
     if (submitButton.length == 0) return alert("No submit button found");
@@ -12,7 +14,23 @@ $("#postTextarea").keyup(event => {
     }
 
     submitButton.prop("disabled", false);
-})
+});
+
+$("#replyTextarea").keyup(event => {
+    const textbox = $(event.target);
+    const value = textbox.val().trim();
+
+    const submitButton = $("#submitReplyButton");
+
+    if (submitButton.length == 0) return alert("No submit button found");
+
+    if (value == "") {
+        submitButton.prop("disabled", true);
+        return;
+    }
+
+    submitButton.prop("disabled", false);
+});
 
 $("#submitPostButton").click(() => {
     const button = $(event.target);
