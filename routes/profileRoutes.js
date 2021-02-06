23ed8a85da784c2451a6735001b0ca78d85986c5
Mohammed Farhan <mohammednm40@gmail.com>
2021-02-06 15:34:47 +0200

    Creating user profile

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -6,13 +6,46 @@ const bcrypt = require("bcrypt");
 const User = require('../schemas/UserSchema');
 
 router.get("/", (req, res, next) => {
-    const payload = {
+
+    let payload = {
         pageTitle: req.session.user.username,
         userLoggedIn: req.session.user,
         userLoggedInJs: JSON.stringify(req.session.user),
-        peofileUSer: req.session.user,
+        profileUser: req.session.user
     }
+
     res.status(200).render("profilePage", payload);
 })
+
+router.get("/:username", async (req, res, next) => {
+
+    const payload = await getPayload(req.params.username, req.session.user);
+
+    res.status(200).render("profilePage", payload);
+});
+
+const getPayload = async (username, userLoggedIn) => {
+    let user = await User.findOne({ username: username });
+
+    if (user == null) {
+
+        user = await User.findById({ username: username })
+
+        if (user == null) {
+            return {
+                pageTitle: "User not found",
+                userLoggedIn: userLoggedIn,
+                userLoggedInJs: JSON.stringify(userLoggedIn)
+            }
+        }
+    }
+
+    return {
+        pageTitle: user.username,
+        userLoggedIn: userLoggedIn,
+        userLoggedInJs: JSON.stringify(userLoggedIn),
+        profileUser: user
+    }
+}
 
 module.exports = router;
