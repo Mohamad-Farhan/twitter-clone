dc765c7bae308d4013370875c46aedf2506e5b75
Mohammed Farhan <mohammednm40@gmail.com>
2021-02-07 22:22:56 +0200

    implementing the tab replies

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,9 +1,22 @@
 $(document).ready(() => {
-    loadPosts();
+
+    const loadReplies = () => {
+        $.get("/api/posts/", { postedBy: profileUserId, isReply: true }, results => {
+            outputPosts(results, $(".postsContainer"));
+        });
+    }
+
+    if (selectedTab === "replies") {
+        loadReplies();
+    } else {
+        loadPosts();
+    }
 });
 
 const loadPosts = () => {
     $.get("/api/posts/", { postedBy: profileUserId, isReply: false }, results => {
         outputPosts(results, $(".postsContainer"));
     });
+
+
 }
